79134a55069f82eda00cb3ea14608d7e3128cd0c
Orla Mallon <52678852+Orlz@users.noreply.github.com>
2021-05-21 09:51:57 +0200

    Update create_virtual_environment.sh

--- create_virtual_environment.sh
+++ create_virtual_environment.sh
@@ -6,12 +6,16 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
+# problems when installing from requirements.txt
 pip install ipython
 pip install jupyter
 pip install tensorflow
+pip install matplotlib
+pip install opencv-python
+pip install pydot
+pip install tqdm
 python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements.txt && pip install -r requirements.txt
-
+test -f requirements.txt && pip install requirements.txt
 deactivate
 echo "build $VENVNAME"
